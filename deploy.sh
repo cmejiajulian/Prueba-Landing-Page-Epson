ce560ef677c70861fa4a052bca66d0269acc5b2e
Julian <cmejiajulian@gmail.com>
2025-08-19 08:21:27 -0500

    Agregar carpeta docs para GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# Elimina docs si ya existe
+rm -rf docs
+
+# Crea carpeta docs
+mkdir docs
+
+# Copia archivos principales
+cp index.html README.md docs/
+
+# Copia carpetas necesarias
+cp -r css fonts img docs/
+
+echo "✅ Proyecto copiado a docs/"
+
+
